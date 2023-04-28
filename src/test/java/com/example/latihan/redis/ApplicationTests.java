1e838c615b98f71dc0667039a0a3ab3258bff850
faturprayuda <muhammad.fathur.prayuda@bni.co.id>
2023-04-28 11:12:23 +0700

    init

---
+++ src/test/java/com/example/latihan/redis/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latihan.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
